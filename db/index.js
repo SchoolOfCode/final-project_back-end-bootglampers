ce5c2090d993f446a6a620c5a6240e7397552550
chocorainaaa <blackettraina@gmail.com>
2022-08-05 14:26:11 +0100

    heroku testing env variables

--- db/index.js
+++ db/index.js
@@ -1,22 +1,12 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  DATABASE_URL: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
 function poolEnvVarCheck() {
-  if (
-    typeof process.env.PGUSER === "undefined" ||
-    typeof process.env.PGHOST === "undefined" ||
-    typeof process.env.PGDATABASE === "undefined" ||
-    typeof process.env.PGPASSWORD === "undefined" ||
-    typeof process.env.PGPORT === "undefined"
-  ) {
+  if (typeof process.env.DATABASE_URL === "undefined") {
     console.log("Some or all enivronment variables missing in Pool");
   }
   console.log("credentials are working");
