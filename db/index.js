f8268ef7e5a63b990dc1d754467536558ab529a8
chocorainaaa <blackettraina@gmail.com>
2022-08-05 19:13:22 +0100

    added config file, db link working with url

--- db/index.js
+++ db/index.js
@@ -1,29 +1,34 @@
 import pg from "pg";
+import { db } from "../config/index.js";
 
 const pool = new pg.Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  ssl: { rejectUnauthorized: false },
+  connectionString: db.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // user: process.env.PGUSER,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  // port: process.env.PGPORT,
+  // ssl: { rejectUnauthorized: false },
 });
 
-function poolEnvVarCheck() {
-  if (
-    typeof process.env.PGUSER === "undefined" ||
-    typeof process.env.PGHOST === "undefined" ||
-    typeof process.env.PGDATABASE === "undefined" ||
-    typeof process.env.PGPASSWORD === "undefined" ||
-    typeof process.env.PGPORT === "undefined"
-  ) {
-    console.log("Some or all enivronment variables missing in Pool");
-  }
-  console.log("credentials are working");
-}
+// function poolEnvVarCheck() {
+//   if (
+//     typeof process.env.PGUSER === "undefined" ||
+//     typeof process.env.PGHOST === "undefined" ||
+//     typeof process.env.PGDATABASE === "undefined" ||
+//     typeof process.env.PGPASSWORD === "undefined" ||
+//     typeof process.env.PGPORT === "undefined"
+//   ) {
+//     console.log("Some or all enivronment variables missing in Pool");
+//   }
+//   console.log("credentials are working");
+// }
 
 export function query(text, params, callback) {
   return pool.query(text, params, callback);
 }
 
-poolEnvVarCheck();
+// poolEnvVarCheck();
