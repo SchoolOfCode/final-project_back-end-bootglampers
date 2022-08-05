e63a843cd66be9967fce9460f17926ed38078631
chocorainaaa <blackettraina@gmail.com>
2022-08-05 10:18:06 +0100

    basic app.js set up. db/index.js config set up with heroku

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import logger from "morgan";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(cors());
+app.use(express.json());
+
+//test route
+app.get("/", function (req, res) {
+  res.json({
+    success: true,
+    message: "Test route up and running!",
+  });
+});
+
+//routes
+
+export default app;
