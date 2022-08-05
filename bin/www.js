6b068e615762aa3473fa85c4ff516b5488dd002e
chocorainaaa <blackettraina@gmail.com>
2022-08-05 14:21:45 +0100

    heroku testing

--- bin/www.js
+++ bin/www.js
@@ -7,7 +7,7 @@ import app from "../app.js";
 
 const debug = debugLib("final-project_back-end-bootglampers");
 
-/*** Get port from environment and store in Express. */
+/*** Get port from environment and store in Express. **/
 
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
