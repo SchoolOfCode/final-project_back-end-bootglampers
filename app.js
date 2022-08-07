6cc770ba054b0edbcdc14946704d2161aee312e9
chocorainaaa <blackettraina@gmail.com>
2022-08-07 19:49:10 +0100

    removed test route in hopes of solving heroku issue

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 // //test route
-app.get("/", function (req, res) {
-  res.json({
-    success: true,
-    message: "Test route up and running!",
-  });
-});
+// app.get("/", function (req, res) {
+//   res.json({
+//     success: true,
+//     message: "Test route up and running!",
+//   });
+// });
 
 // // routes
 app.use("/stats", statsRouter);
