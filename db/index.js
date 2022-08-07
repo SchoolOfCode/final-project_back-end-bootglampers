55eb58a442c85683466aa47ee06597c9af25613f
chocorainaaa <blackettraina@gmail.com>
2022-08-07 21:05:26 +0100

    try remove pg from devdepneicies

--- db/index.js
+++ db/index.js
@@ -6,29 +6,8 @@ const pool = new pg.Pool({
   ssl: {
     rejectUnauthorized: false,
   },
-  // user: process.env.PGUSER,
-  // host: process.env.PGHOST,
-  // database: process.env.PGDATABASE,
-  // password: process.env.PGPASSWORD,
-  // port: process.env.PGPORT,
-  // ssl: { rejectUnauthorized: false },
 });
-
-// function poolEnvVarCheck() {
-//   if (
-//     typeof process.env.PGUSER === "undefined" ||
-//     typeof process.env.PGHOST === "undefined" ||
-//     typeof process.env.PGDATABASE === "undefined" ||
-//     typeof process.env.PGPASSWORD === "undefined" ||
-//     typeof process.env.PGPORT === "undefined"
-//   ) {
-//     console.log("Some or all enivronment variables missing in Pool");
-//   }
-//   console.log("credentials are working");
-// }
 
 export function query(text, params, callback) {
   return pool.query(text, params, callback);
 }
-
-// poolEnvVarCheck();
