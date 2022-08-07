56028f70382d5b6c922b9300a08e4fcdf31f4b39
chocorainaaa <blackettraina@gmail.com>
2022-08-07 15:53:21 +0100

    switch stament for stats page route created with two get requests - total visits and total medi time

--- models/index.js
+++ models/index.js
@@ -3,20 +3,28 @@ import { query } from "../db/index.js";
 //************** Get all stats **********/
 // paramater of userid
 // visits
-export async function getTotalVisits(req) {
-  const userId = `'${req.firebaseId}'`;
-  // CNXBkvXJbxUjh5bOxk8NN2DV2l72
+export async function getTotalVisits(userId) {
   console.log(userId);
   const result = await query(
-    `
-        SELECT total_visits FROM users
-        WHERE firebase_user_id = $1;
-    `,
+    `SELECT total_visits FROM users
+    WHERE firebase_user_id = $1;`,
     [userId]
   );
   return result.rows;
 }
 
+export async function getTotalMedTime(userId) {
+  console.log(userId);
+  const result = await query(
+    `SELECT SUM(meditation_length)
+    FROM meditation_log AS m
+    LEFT JOIN users AS u
+    ON u.user_id = m.user_id 
+    WHERE u.firebase_user_id = $1;`,
+    [userId]
+  );
+  return result.rows;
+}
 // total meditation time
 
 // streak
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,37 @@
 import express from "express";
 import { getTotalVisits } from "../models/index.js";
+import { getTotalMedTime } from "../models/index.js";
 
 const router = express.Router();
 
-router.get("/visits", async function (req, res) {
-  const result = await getTotalVisits(req);
-  res.json({
-    success: true,
-    payload: result,
-  });
+router.get("/:userId/:path", async function (req, res) {
+  // example id = CNXBkvXJbxUjh5bOxk8NN2DV2l72
+  // returns total_visits:
+  // const userId = req.query.id;
+  console.log(req.params.userId);
+  console.log(req.params.param2);
+  let result = {};
+  switch (req.params.path) {
+    case "visits":
+      result = await getTotalVisits(req.params.userId);
+      console.log(req.params.userId);
+      res.json({
+        success: true,
+        payload: result,
+      });
+      break;
+    case "totalmedtime":
+      result = await getTotalMedTime(req.params.userId);
+      console.log(req.params.userId);
+      res.json({
+        success: true,
+        payload: result,
+      });
+
+    default:
+      console.log("error");
+      break;
+  }
 });
 
 export { router as statsRouter };
