5ba24e1a37415b824159651c888fac923e26cac1
chocorainaaa <blackettraina@gmail.com>
2022-08-07 16:14:58 +0100

    removing cookie parser in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // routes
 // app.use("/stats", statsRouter);
