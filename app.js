187dd0b5b0c91dfc1b5fdac57369dcc7d20cc435
chocorainaaa <blackettraina@gmail.com>
2022-08-07 16:11:16 +0100

    removed express urlencoded in app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // routes
