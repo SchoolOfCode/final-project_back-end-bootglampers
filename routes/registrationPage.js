b48721c96e23215d810122a227601ef8561dbf0b
chocorainaaa <blackettraina@gmail.com>
2022-08-09 14:35:22 +0100

    removed createrUserEntry from registration route to test whether post request of pet name can be sent when called individually

--- routes/registrationPage.js
+++ routes/registrationPage.js
@@ -8,7 +8,7 @@ const registrationRouter = express.Router();
 
 registrationRouter.post("/", async function (req, res) {
   const result = {
-    created_user_data: await createUserEntry(req),
+    // created_user_data: await createUserEntry(req),
     created_pet_data: await createPetEntry(req),
   };
   res.json({
