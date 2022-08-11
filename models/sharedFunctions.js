ea9df00f451fee80e8e948d7dfdedf2d4f1f4ad6
chocorainaaa <blackettraina@gmail.com>
2022-08-11 14:23:46 +0100

    changed getpet id route to retrive a name so we can check for null

--- models/sharedFunctions.js
+++ models/sharedFunctions.js
@@ -22,3 +22,16 @@ export async function getPetId(firebaseUserId) {
   const petId = result.rows[0].pet_id;
   return petId;
 }
+
+export async function getPetEntry(firebaseUserId) {
+  const result = await query(
+    `SELECT u.username, u.user_id, p.pet_id, p.pet_name 
+    FROM pets AS p
+    LEFT JOIN users AS u
+    ON u.user_id = p.user_id
+    WHERE u.firebase_user_id = $1;`,
+    [firebaseUserId]
+  );
+  const petId = result.rows[0].pet_name;
+  return petId;
+}
--- routes/getPetId.js
+++ routes/getPetId.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getPetId } from "../models/sharedFunctions.js";
+import { getPetEntry } from "../models/sharedFunctions.js";
 
 const petIdRouter = express.Router();
 
 petIdRouter.get("/:userId", async function (req, res) {
   const userId = req.params.userId;
-  const result = await getPetId(userId);
+  const result = await getPetEntry(userId);
   res.json({
     success: true,
     pet_id: result,
