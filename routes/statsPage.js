262509c4bc99814e9bdf0d876a005abcfcda5b02
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-08-22 18:38:26 +0100

    added a error handling to the stats get request

--- routes/statsPage.js
+++ routes/statsPage.js
@@ -23,51 +23,55 @@ statsRouter.get("/:userId", async function (req, res) {
     dailyStreakPromise,
     avgMoodPromise,
     allMoodLogsPromise,
-  ]).then((values) => {
-    const result = {
-      visits: values[0],
-      total_meditation_time: values[1],
-      daily_streak: values[2],
-      mood_data: {
-        average_mood: values[3],
-        all_moodlogs: values[4],
-      },
-    };
+  ]).catch((e) => {
     res.json({
-      success: true,
-      payload: result,
+      success: false,
+      payload: e,
+    }).then((values) => {
+      const result = {
+        visits: values[0],
+        total_meditation_time: values[1],
+        daily_streak: values[2],
+        mood_data: {
+          average_mood: values[3],
+          all_moodlogs: values[4],
+        },
+      };
+      res.json({
+        success: true,
+        payload: result,
+      });
     });
   });
-});
 
-// try {
-//   await Promise.all([
-//     visitsPromise,
-//     totalMeditationTimePromise,
-//     dailyStreakPromise,
-//     avgMoodPromise,
-//     allMoodLogsPromise,
-//   ]).then((values) => {
-//     const result = {
-//       visits: values[0],
-//       total_meditation_time: values[1],
-//       daily_streak: values[2],
-//       mood_data: {
-//         average_mood: values[3],
-//         all_moodlogs: values[4],
-//       },
-//     };
-//     res.json({
-//       success: true,
-//       payload: result,
-//     });
-//   });
-// } catch (e) {
-//   res.json({
-//     success: false,
-//     payload: e,
-//   });
-// }
-// });
+  // try {
+  //   await Promise.all([
+  //     visitsPromise,
+  //     totalMeditationTimePromise,
+  //     dailyStreakPromise,
+  //     avgMoodPromise,
+  //     allMoodLogsPromise,
+  //   ]).then((values) => {
+  //     const result = {
+  //       visits: values[0],
+  //       total_meditation_time: values[1],
+  //       daily_streak: values[2],
+  //       mood_data: {
+  //         average_mood: values[3],
+  //         all_moodlogs: values[4],
+  //       },
+  //     };
+  //     res.json({
+  //       success: true,
+  //       payload: result,
+  //     });
+  //   });
+  // } catch (e) {
+  //   res.json({
+  //     success: false,
+  //     payload: e,
+  //   });
+  // }
+  // });
 
-export default statsRouter;
+  export default statsRouter;
